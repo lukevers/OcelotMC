79592d6e9b78642bba39c0095ea7df11b81a2c5b
Luke Evers <me@lukevers.com>
2013-05-01 07:52:19 -0400

    Removed the class file

--- src/ocelot/Ocelot.java
+++ src/ocelot/Ocelot.java
@@ -7,15 +7,16 @@ import org.bukkit.plugin.java.JavaPlugin;
 public final class Ocelot extends JavaPlugin {
 	
 	private Logger log = Logger.getLogger("Minecraft");
+	private String Version;
 
 	@Override 
 	public void onEnable() {
-		log.info("Ocelot enabled");
+		log.info("Ocelot "+Version+" enabled");
 	} // close on enable
 	
 	@Override
 	public void onDisable() {
-		log.info("Ocelot disabled");
+		log.info("Ocelot "+Version+" disabled");
 	} // close on disable
 	
 } // close class
