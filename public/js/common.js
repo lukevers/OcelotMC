12c9486e65cba41c963d7cd1dc4a7598cb3cda00
Luke Evers <me@lukevers.com>
2013-05-02 12:07:14 -0400

    Still haven't tested refreshing or updating, but progress.

--- public/js/common.js
+++ public/js/common.js
@@ -2,3 +2,17 @@
 //	Ocelot
 //	common.js
 //
+
+function create(yaml) {
+	var config = jsyaml.load(yaml);	
+	
+	for (var user in config) {
+		var health = config[user].health;
+		var online = config[user].online;
+		var x = config[user].location.x;
+		var y = config[user].location.y;
+		var z = config[user].location.x;
+		var foodlevel = config[user].foodlevel;
+	}
+
+}
