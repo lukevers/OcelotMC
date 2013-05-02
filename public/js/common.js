f733638603ce21f264b2e395873222696f87b263
Luke Evers <me@lukevers.com>
2013-05-02 19:59:01 -0400

    Added support for only showing online people

--- public/js/common.js
+++ public/js/common.js
@@ -8,17 +8,19 @@ function update(yaml) {
 	$('.users').empty();
 	
 	for (var user in config) {	
-		$('.users').append(
-		'<div class="user ' + user + '">' +
-			'<div class="name">' + user + '</div>' +
-			'<div class="health">' + config[user].health + '</div>' +
-			'<div class="food">' + config[user].foodlevel + '</div>' +
-			'<div class="location">' +
-				'<div class="x">' + config[user].location.x + '</div>' +
-				'<div class="y">' + config[user].location.y + '</div>' +
-				'<div class="z">' + config[user].location.z + '</div>' +
-			'</div>' +
-		'</div>'
-		);
+		if (config[user].online == true) {
+			$('.users').append(
+			'<div class="user ' + user + '">' +
+				'<div class="name">' + user + '</div>' +
+				'<div class="health">' + config[user].health + '</div>' +
+				'<div class="food">' + config[user].foodlevel + '</div>' +
+				'<div class="location">' +
+					'<div class="x">' + config[user].location.x + '</div>' +
+					'<div class="y">' + config[user].location.y + '</div>' +
+					'<div class="z">' + config[user].location.z + '</div>' +
+				'</div>' +
+			'</div>'
+			);
+		}
 	} // close for loop
 } // close create
