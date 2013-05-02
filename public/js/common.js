4dfce861ec73f850d26e09df03ba051634e6e1f1
Luke Evers <me@lukevers.com>
2013-05-02 19:56:21 -0400

    Added the users name

--- public/js/common.js
+++ public/js/common.js
@@ -9,13 +9,14 @@ function update(yaml) {
 	
 	for (var user in config) {	
 		$('.users').append(
-		'<div class="user '+user+'">' +
-			'<div class="health">'+config[user].health+'</div>' +
-			'<div class="food">'+config[user].foodlevel+'</div>' +
+		'<div class="user ' + user + '">' +
+			'<div class="name">' + user + '</div>' +
+			'<div class="health">' + config[user].health + '</div>' +
+			'<div class="food">' + config[user].foodlevel + '</div>' +
 			'<div class="location">' +
-				'<div class="x">'+config[user].location.x+'</div>' +
-				'<div class="y">'+config[user].location.y+'</div>' +
-				'<div class="z">'+config[user].location.z+'</div>' +
+				'<div class="x">' + config[user].location.x + '</div>' +
+				'<div class="y">' + config[user].location.y + '</div>' +
+				'<div class="z">' + config[user].location.z + '</div>' +
 			'</div>' +
 		'</div>'
 		);
